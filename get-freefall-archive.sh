7685ffc1c14db2c81822fcfba33af84f0e12e678
Sören Köhler <soerenkoehler@users.noreply.github.com>
2017-05-25 01:33:32 +0200

    Freefall-Archivierer, Bilder speichern

--- get-freefall-archive.sh
+++ get-freefall-archive.sh
@@ -1,20 +1,38 @@
 #!/bin/sh
 
-getPage() {
+download() {
 	if which wget >/dev/null 2>/dev/null; then
-		wget "$BASE_URL$1" -o $TEMP_FILE
+		wget "$BASE_URL$1" >$2
 		return
 	elif which curl >/dev/null 2>/dev/null; then
-		curl "$BASE_URL$1" -s -o $TEMP_FILE
+		curl "$BASE_URL$1" -s >$2
 		return
 	fi
 	echo please install wget or curl >&2
 	exit -1
 }
 
-getURL() {
+downloadPage() {
+	download $1 "./$TEMP_FILE"
+}
+
+downloadImage() {
+	download $1 "./$TEMP_IMAGE"
+	mkdir -p $(dirname "./$1")
+	mv -T "./$TEMP_IMAGE" "./$1"
+}
+
+getPageURL() {
 	awk -v urlPattern="$1" '{
 		if( match(tolower($0), "href=\"(.+)\">" tolower(urlPattern), result) ) {
+			print substr($0, result[1, "start"], result[1, "length"]);
+		}
+	}' $TEMP_FILE;
+}
+
+getImageURL() {
+	awk '{
+		if( match(tolower($0), "<img src=\"(.+)\"", result) ) {
 			print substr($0, result[1, "start"], result[1, "length"]);
 		}
 	}' $TEMP_FILE;
@@ -34,16 +52,18 @@ updateURL() {
 }
 
 TEMP_FILE="tmp.html"
+TEMP_IMAGE="tmp.gif"
 BASE_URL="http://freefall.purrsia.com"
 CURRENT_URL="/grayff.htm"
 
-getPage $CURRENT_URL
-CURRENT_URL=$(updateURL $CURRENT_URL $(getURL "Story Start"))
+downloadPage $CURRENT_URL
+CURRENT_URL=$(updateURL $CURRENT_URL $(getPageURL "Story Start"))
 
 while [ -n $CURRENT_URL ]; do
 	echo $CURRENT_URL
-	getPage $CURRENT_URL
-	CURRENT_URL=$(updateURL $CURRENT_URL $(getURL "Next"))
+	downloadPage $CURRENT_URL
+	downloadImage $(updateURL $CURRENT_URL $(getImageURL))
+	CURRENT_URL=$(updateURL $CURRENT_URL $(getPageURL "Next"))
 done
 
 rm $TEMP_FILE
